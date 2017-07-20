fab11754360fcb243e0947ca74bd58e568b7d9f0
LAB SD <28992896+LABhf@users.noreply.github.com>
2017-07-20 13:59:47 -0300

    file name and path changes

---
+++ aviso.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+
+now = datetime.datetime.now()
+
+print(now)
+print(" ")
+print("PrinterDontDry")
+print(" ")
+print("Printing paper, so that the ink dosent dry..")
+print(" ")
+os.system("pause")
+os.startfile("A1.docx", "print")
